408eefea48c9e6f48619191634a459620bbd2dd0
Farhan <157615419+frhnx11@users.noreply.github.com>
2025-08-21 18:33:36 +0530

    Create project-2/src/App.jsx

---
+++ project-2/src/App.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  font-family: 'Roboto', sans-serif;
+  background-color: #f0f8ff;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.header`
+  background-color: #ff6b6b;
+  width: 100%;
+  padding: 20px 0;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-family: 'Pacifico', cursive;
+  color: #fff;
+  font-size: 3rem;
+  margin: 0;
+`;
+
+const MainContent = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  text-align: center;
+`;
+
+const Headline = styled.h2`
+  color: #4169e1;
+  font-size: 2.5rem;
+  margin-bottom: 20px;
+`;
+
+const Subheadline = styled.p`
+  color: #333;
+  font-size: 1.2rem;
+  max-width: 600px;
+  margin-bottom: 30px;
+`;
+
+const CTAButton = styled.button`
+  background-color: #4169e1;
+  color: #fff;
+  border: none;
+  padding: 15px 30px;
+  font-size: 1.2rem;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #1e90ff;
+  }
+`;
+
+const App = () => {
+  return (
+    <AppContainer>
+      <Header>
+        <Logo>Merry Berry</Logo>
+      </Header>
+      <MainContent>
+        <Headline>Adorable Clothes for Your Little Ones</Headline>
+        <Subheadline>
+          Discover our colorful collection of comfortable and stylish baby clothes that will make your child stand out!
+        </Subheadline>
+        <CTAButton>Shop Now</CTAButton>
+      </MainContent>
+    </AppContainer>
+  );
+};
+
+export default App;
