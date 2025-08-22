254be1f56bb60b930a10dd6d763bc93afaa78b92
Farhan <157615419+frhnx11@users.noreply.github.com>
2025-08-22 18:56:14 +0530

    Delete project-5/src/components/Footer.tsx

--- project-5/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2023 E-commerce Store. All rights reserved.</p>
-      </div>
-    </footer>
-  )
-}
